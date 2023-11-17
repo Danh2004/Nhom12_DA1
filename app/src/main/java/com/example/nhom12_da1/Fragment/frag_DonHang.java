e602122a6de3815adab570700b4ca776d7aae38a
Danh2004 <anhvtdph40034@fpt.edu.vn>
2023-11-17 19:28:13 +0700

    bắt đầu dự án

--- app/src/main/java/com/example/nhom12_da1/Fragment/frag_DonHang.java
+++ app/src/main/java/com/example/nhom12_da1/Fragment/frag_DonHang.java
@@ -1,4 +1,27 @@
 package com.example.nhom12_da1.Fragment;
 
-public class frag_DonHang {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.nhom12_da1.R;
+
+public class frag_DonHang extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v  = inflater.inflate(R.layout.frag_donhang,container,false);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/nhom12_da1/Fragment/frag_ThanhVien.java
+++ app/src/main/java/com/example/nhom12_da1/Fragment/frag_ThanhVien.java
@@ -1,4 +1,30 @@
 package com.example.nhom12_da1.Fragment;
 
-public class frag_ThanhVien {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nhom12_da1.R;
+
+public class frag_ThanhVien extends Fragment {
+    RecyclerView lvThanhVien;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frag_thanhvien,container,false);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
