f1d38e244a2fa501eb40b67cf35ee09f4c097515
Danh2004 <anhvtdph40034@fpt.edu.vn>
2023-11-17 19:40:29 +0700

    màn hình chờ

---
+++ app/src/main/java/com/example/nhom12_da1/ManCho_Activity.java
@@ -0,0 +1,14 @@
+package com.example.nhom12_da1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManCho_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_cho);
+    }
+}
