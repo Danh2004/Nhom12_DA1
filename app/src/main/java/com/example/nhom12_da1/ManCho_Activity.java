349073a5ded5f68bae8e9a0edbe98f3a3ddd4af0
Giappp2004 <giapvxph34922@fpt.edu.vn>
2023-11-17 19:51:10 +0700

    manhinhcho

--- app/src/main/java/com/example/nhom12_da1/ManCho_Activity.java
+++ app/src/main/java/com/example/nhom12_da1/ManCho_Activity.java
@@ -2,7 +2,9 @@ package com.example.nhom12_da1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class ManCho_Activity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ManCho_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_cho);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ManCho_Activity.this, Login_Activity.class);
+                startActivity(intent);
+            }
+        }, 3500);
     }
 }
