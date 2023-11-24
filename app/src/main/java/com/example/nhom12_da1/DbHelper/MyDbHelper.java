e8ae2a4628572004b7743395231ef3f6e2cad9f8
Danh2004 <anhvtdph40034@fpt.edu.vn>
2023-11-24 20:11:39 +0700

    sửa lỗi spinner 2

--- app/src/main/java/com/example/nhom12_da1/DbHelper/MyDbHelper.java
+++ app/src/main/java/com/example/nhom12_da1/DbHelper/MyDbHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDbHelper extends SQLiteOpenHelper {
     static final String dbName = "GiaySneaker";
-    static  final  int  version=13;
+    static  final  int  version=14;
     public MyDbHelper(Context context){
         super(context,dbName,null,version);
     }
@@ -59,8 +59,8 @@ public class MyDbHelper extends SQLiteOpenHelper {
         db.execSQL("insert into ThanhVien values('1','Vo Duy Anh','01234567890','Ha Noi')," +
                 "('2','Vu Xuan Giap','01234567891','Ha Noi')," +
                 "('3','Le Minh Chien','01234567892','Ha Noi')");
-        db.execSQL("insert into DonHang values('1','AirJordan','39','1','40000','Nike')," +
-                "('2','AirForce','43','4','160000','Adidas')" );
+        db.execSQL("insert into DonHang values('1','1','AirJordan','39','1','40000','Nike')," +
+                "('2','2','AirForce','43','4','160000','Adidas')" );
         db.execSQL("insert into GioHang values('1','AirJordan','39','40000')," +
                 "('2','AirForce','43','160000')");
 
